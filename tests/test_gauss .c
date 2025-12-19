68f611b30fa8d12bfa99d9377086986c4b7097c3
Ryhor_Aliferchyk <01205860@pw.edu.pl>
2025-12-19 02:13:42 +0100

    Poprawki w testach eliminacji Gaussa

--- tests/test_gauss .c
+++ tests/test_gauss .c
@@ -1,3 +1,26 @@
-//
-// Created by GGY on 19/12/2025.
-//
+#include <stdio.h>
+#include "../src/gauss.h"
+
+int main() {
+    int n = 3;
+
+    double A[3][3] = {
+        { 2, 1, -1 },
+        { -3, -1, 2 },
+        { -2, 1, 2 }
+    };
+
+    double b[3] = { 8, -11, -3 };
+
+    gaussian_elimination(n, A, b);
+
+    printf("Po eliminacji Gaussa:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%8.3f ", A[i][j]);
+        }
+        printf("| %8.3f\n", b[i]);
+    }
+
+    return 0;
+}
