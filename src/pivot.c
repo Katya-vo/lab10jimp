db792e9e9c5eb6062dedfbde0f6e981bb31de502
Ryhor_Aliferchyk <01205860@pw.edu.pl>
2025-12-19 01:25:49 +0100

    Zmiana wyboru elementu glownego (wersja A)

--- src/pivot.c
+++ src/pivot.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 void choose_pivot(int n, double A[n][n], int k) {
-    // wersja A – pusta implementacja
-    printf("Pivot A\n");
+    // wersja A
+    printf("Pivot chosen by version A\n");
 }
